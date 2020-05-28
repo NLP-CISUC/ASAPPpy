926a27d74776169d6faba1f3682d64b762dcde13
ZPedroP <santos.14.pedro@gmail.com>
2020-05-28 10:23:37 +0100

    added separate script to load the models of embeddings

---
+++ ASAPPpy/scripts/load_embeddings.py
@@ -0,0 +1,58 @@
+import os
+import time
+from gensim.models import KeyedVectors
+
+def load_embeddings_models():
+	""" Function used to load the word-embedding models """
+
+	# ---LOADING WORD2VEC MODEL---
+	model_load_path = os.path.join('models', 'word2vec', 'NILC', 'nilc_cbow_s300_300k.txt')
+	start_time = time.time()
+	print("Started loading the word2vec model")
+	word2vec_model = KeyedVectors.load_word2vec_format(model_load_path)
+	# word2vec_model = None
+	print("Model loaded")
+	print("--- %s seconds ---" %(time.time() - start_time))
+	print('\a')
+
+	# ---LOADING FASTTEXT MODEL---
+	model_path = os.path.join('models', 'fastText', 'cc.pt.300_300k.vec')
+	start_time = time.time()
+	print("Started loading the fasttext model")
+	fasttext_model = KeyedVectors.load_word2vec_format(model_path)
+	# fasttext_model = None
+	print("Model loaded")
+	print("--- %s seconds ---" %(time.time() - start_time))
+	print('\a')	
+
+	# ---LOADING PT-LKB MODEL---
+	model_load_path = os.path.join('models', 'ontoPT', 'PT-LKB_embeddings_64', 'ptlkb_64_30_200_p_str.emb')
+	start_time = time.time()
+	print("Started loading the PT-LKB-64 model")
+	ptlkb64_model = KeyedVectors.load_word2vec_format(model_load_path)
+	# ptlkb64_model = None
+	print("Model loaded")
+	print("--- %s seconds ---" %(time.time() - start_time))
+	print('\a')
+
+	# ---LOADING GLOVE-300 MODEL---
+	model_load_path = os.path.join('models', 'glove', 'glove_s300_300k.txt')
+	start_time = time.time()
+	print("Started loading the GLOVE 300 dimensions model")
+	glove300_model = KeyedVectors.load_word2vec_format(model_load_path)
+	# glove300_model = None
+	print("Model loaded")
+	print("--- %s seconds ---" %(time.time() - start_time))
+	print('\a')
+
+	# ---LOADING NUMBERBATCH MODEL---
+	model_load_path = os.path.join('models', 'numberbatch', 'numberbatch-17.02_pt_tratado.txt')
+	start_time = time.time()
+	print("Started loading the NUMBERBATCH dimensions model")
+	numberbatch_model = KeyedVectors.load_word2vec_format(model_load_path)
+	# numberbatch_model = None
+	print("Model loaded")
+	print("--- %s seconds ---" %(time.time() - start_time))
+	print('\a')
+
+	return word2vec_model, fasttext_model, ptlkb64_model, glove300_model, numberbatch_model
